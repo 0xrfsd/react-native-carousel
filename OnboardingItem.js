758c5973facab1c2d52aa50965bfd825e21702cb
0xrfsd <64338100+0xrfsd@users.noreply.github.com>
2021-02-16 15:24:49 -0300

    V1() => Image Slide Width Fixed!
    :)

--- OnboardingItem.js
+++ OnboardingItem.js
@@ -4,10 +4,11 @@ import { View, Text, StyleSheet, Image, useWindowDimensions } from 'react-native
 
 export default OnboardingItem = ({ item }) => {
 
+    const windowWidth = useWindowDimensions().width;
 
     return (
-        <View style={{ paddingTop: 100}}>
-                <Image source={item.image} style={{ height: 250, width: '100%' }} />
+        <View style={[{ flex: 1, paddingTop: 25, alignItems: 'center', paddingLeft: 10, paddingRight: 10 }, {windowWidth}]}>
+                <Image source={item.image}style={{ flex: 0.5, width: '100%', justifyContent: 'center', borderRadius: 10, resizeMode: 'cover' }} />
                 <Text>{item.description}</Text>
                 <Text>{item.title}</Text>
         </View>
